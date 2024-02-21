e61bd7cec1f9e1617b6ea3267bbb26bc49abdbcc
Vancheres <anisii840@gmail.com>
2024-02-21 08:43:50 +0500

    log

---
+++ ft-220008 Anisimov.py
@@ -0,0 +1,6 @@
+import random
+import logging
+from colorama import Fore
+
+# Настройка логирования
+logging.basicConfig(filename='game.log', level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s')
